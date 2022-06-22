a3a8948cc7eeffc21324ad41d066a219c5487438
jaime.garita <jaime.garita.camargo@gmail.com>
2022-06-22 23:54:43 +0200

    Exercises Bubble Int

--- Array/src/BubbleSortInt.java
+++ Array/src/BubbleSortInt.java
@@ -29,24 +29,24 @@ public class BubbleSortInt {
         System.out.println("\n\n");
         //Other solution
         Integer[] num2 = new Integer[4];
-        for(int i = 0; i<num2.length; i++) {
-            int rand = ranObj.nextInt();
-            num2[i] = rand;
-            System.out.println("num2[i] = " + num2[i]);
-        }
+        num2[0] = 1;
+        num2[1] = 60;
+        num2[2] = 3;
+        num2[3] = 10;
+
+        System.out.println("num2 = " + num2);
+
         System.out.println("\n\n");
 
-        for(int i = 0; i<num2.length; i++){
-            for(int j = 0; j<num2.length-1-i;j++){
-              if(num2[j+1].compareTo(num2[j]) > 0){
-                  int aux = num2[j];
-                  num2[j] = num2[j+1];
-                  num2[j+1] = aux;
-
-              }
-
+        for(int i = 0; i< num2.length; i++){
+            for(int j = 0; j<num2.length -1 -i; j++){
+                if((Integer) num2[j+1].compareTo(num2[j]) > 0){
+                    Integer aux = num2[j];
+                    num2[j] = num2[j+1];
+                    num2[j+1] = aux;
+                }
             }
-            System.out.println("num2[i] = " + num2[i]);
+            System.out.println("num2[i] " + num2[i]);
         }
 
     }
