47f3ce03a5916c399f7d32322e3d11e75fb63812
jaime.garita <jaime.garita.camargo@gmail.com>
2022-07-08 18:25:32 +0200

    Enum using default type car

---
+++ POO/src/CarTypes.java
@@ -0,0 +1,31 @@
+public enum CarTypes {
+    SEDAN("Sedan", "Medium Car", 4),
+    STATION_WAGON("Station Wagon", "Big Car", 4),
+    HATCHBACK("Hatchback", "Compact Auto", 4),
+    PICKUP("Pickup", "Van", 4),
+    COUPE("Coupe", "Small Car", 2),
+    CONVERTIBLE ("Convertible", "Sport auto", 2),
+    VAN ("Van", "Utility car", 3);
+
+    private final String name;
+    private final int nDoors;
+    private final String description;
+
+    CarTypes(String name, String description, int nDoors) {
+        this.name = name;
+        this.nDoors = nDoors;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getnDoors() {
+        return nDoors;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
