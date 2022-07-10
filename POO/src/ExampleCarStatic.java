c689704cf19310e05d591fddb98583c3e5b7d951
jaime.garita <jaime.garita.camargo@gmail.com>
2022-07-10 23:00:00 +0200

    Class car static using static and methods

--- POO/src/ExampleCarStatic.java
+++ POO/src/ExampleCarStatic.java
@@ -2,34 +2,41 @@
 
 public class ExampleCarStatic {
     public static void main(String[] args) {
-        Car car = new Car("Ferrari", "720", Color.BLANK, 10, 30);
-        Car car2 = new Car("Ferrari", "720", Color.RED, 10, 30);
+        Engine carEngine = new Engine(3.0, TypeEngine.GASOLINE);
+
+        Car car = new Car("Ferrari", "720", Color.BLANK,  new Engine(carEngine), new Deposit());
+        Car lamborghini = new Car("Lamborghini", "Urus", Color.RED, new Engine(4.0, TypeEngine.GASOLINE), new Deposit());
 
         Car.setDefaultColor(Color.YELLOW);
 
-        car2.setType(CarTypes.HATCHBACK);
+        lamborghini.setType(CarTypes.HATCHBACK);
         car.setType(CarTypes.COUPE);
 
         CarTypes carTypes = car.getType();
         System.out.println("Subaru type: " + carTypes.getName());
         System.out.println("Subaru type: " + carTypes.getDescription());
 
-        Car car3 = new Car();
+        lamborghini.setDeposit(new Deposit(40));
+
 
         car.setColor(Color.BLANK);
-        car2.setColor(Color.GREEN);
+        lamborghini.setColor(Color.GREEN);
 
-        System.out.println("car3 = " + car3);
         System.out.println(car.showDetails());
         System.out.println(car.accelerate(100));
         System.out.println(car.curb());
 
         System.out.println("================");
-        System.out.println(car2.showDetails());
-        System.out.println(car2.accelerate(100));
-        System.out.println(car2.curb());
+        System.out.println(lamborghini.showDetails());
+        System.out.println(lamborghini.accelerate(100));
+        System.out.println(lamborghini.curb());
 
         System.out.println("Car.getDefaultColor() = " + Car.getDefaultColor());
+        
+        
+        CarTypes lamborghiniType = lamborghini.getType();
+        System.out.println("lamborghiniType = " + lamborghiniType.getName());
 
+        System.out.println(lamborghini.calculateConsume(300,70));
     }
 }
