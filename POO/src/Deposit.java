a3afa242bd08573e4b0b881ce6b01fd90d90d6aa
jaime.garita <jaime.garita.camargo@gmail.com>
2022-07-10 22:55:53 +0200

    Class deposit

---
+++ POO/src/Deposit.java
@@ -0,0 +1,17 @@
+public class Deposit {
+    private int capacity;
+
+    public Deposit(){
+        this.capacity = 40;
+    }
+
+    public Deposit(int capacity) {
+
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+
+        return capacity;
+    }
+}
