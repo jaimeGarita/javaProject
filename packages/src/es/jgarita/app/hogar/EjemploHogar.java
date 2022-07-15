5c554208439010231de8a7cdddfc830dd89c2c0f
jaime.garita <jaime.garita.camargo@gmail.com>
2022-07-15 22:07:39 +0200

    Exercises using Packages

---
+++ packages/src/es/jgarita/app/hogar/EjemploHogar.java
@@ -0,0 +1,13 @@
+package es.jgarita.app.hogar;
+
+import es.jgarita.app.jardin.Perro;
+import static es.jgarita.app.hogar.Persona.*; //EL ASTERISCO NO CONSUME MAS AÑADIENDOLO QUE SIN AÑADIR
+public class EjemploHogar {
+    public static void main(String[] args) {
+        Persona p = new Persona();
+        Perro perro = new Perro();
+
+        String saludo = saludar();
+
+    }
+}
