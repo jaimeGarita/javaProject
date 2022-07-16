12959e1de7e8d90ccc4da011331f8259e166fe15
jaime.garita <jaime.garita.camargo@gmail.com>
2022-07-16 20:10:25 +0200

    Class cliente

---
+++ appfacturas/src/es/jgarita/appfacturas/model/Cliente.java
@@ -0,0 +1,31 @@
+package es.jgarita.appfacturas.model;
+
+public class Cliente {
+    private String nombre;
+    private String nif;
+
+    private Cliente(){
+        // = Delete in Java
+    }
+
+    public Cliente(String nombre, String nif) {
+        this.nombre = nombre;
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+}
