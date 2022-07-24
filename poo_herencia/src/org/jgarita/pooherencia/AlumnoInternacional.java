7cae9123cb648816d72cc4533fdf99e9e75b355d
jaime.garita <jaime.garita.camargo@gmail.com>
2022-07-24 23:14:52 +0200

    class AlumnoInternacional

--- poo_herencia/src/org/jgarita/pooherencia/AlumnoInternacional.java
+++ poo_herencia/src/org/jgarita/pooherencia/AlumnoInternacional.java
@@ -32,4 +32,24 @@ public class AlumnoInternacional extends Alumno{
     public void setNotaIdiomas(double notaIdiomas) {
         this.notaIdiomas = notaIdiomas;
     }
+
+    @Override
+    public String saludar() {
+        String saludar = super.saludar();
+
+        return saludar + " soy un alumno extranjero del pais " + getPais() + " mi nombre " + getNombre();
+    }
+
+    @Override
+    public double calcularPromedio() {
+        System.out.println("Calcular promedio: " + AlumnoInternacional.class.getCanonicalName());
+        return ((super.calcularPromedio()*3) + notaIdiomas)/4;
+    }
+
+    @Override
+    public String toString() {
+        return  super.toString() +
+                "\n pais='" + pais + '\'' +
+                ", notaIdiomas=" + notaIdiomas;
+    }
 }
